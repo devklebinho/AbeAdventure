af0cf5abfd22e2466256594f32d105b9f38632a9
devklebinho <kleber.silva.kgns@gmail.com>
2023-12-02 12:09:46 -0300

    Patrol behavior

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -5,15 +5,74 @@ using UnityEngine.AI;
 
 public class FollowTarget : MonoBehaviour
 {
+    public float enemyLife;
+
     public Transform[] target;
     public NavMeshAgent agent;
+    public int currentTarget;
+    public float pursuitRadius;
+    public Transform playerReference;
     void Start()
     {
+        playerReference = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        agent.SetDestination(target[0].position);
+        
+        if(enemyLife > 0)
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, playerReference.position);
+
+            if(distanceToPlayer <= agent.stoppingDistance)
+            {
+                //Attack();
+            }
+            else if(distanceToPlayer <= pursuitRadius)
+            {
+                //Pursuit();
+            }
+            else
+            {
+                //Patrol();
+            }
+        }
+        else
+        {
+            Debug.Log("IH morreu");
+        }
+
+    }
+
+    void Attack()
+    {
+        Debug.Log("Atacando");
+    }
+
+    void Pursuit(Transform newTarget)
+    {
+        agent.SetDestination(newTarget.position);
+    }
+
+    void Patrol()
+    {
+        if(!agent.pathPending && agent.remainingDistance < 1f)
+        {
+            GoToNextPoint();
+        }
+    }
+
+    void GoToNextPoint()
+    {
+        if (target.Length == 0)
+            return;
+
+        currentTarget += 1;
+
+        if(currentTarget >= target.Length)
+            currentTarget = 0;
+
+        agent.SetDestination(target[currentTarget].position);
     }
 }
